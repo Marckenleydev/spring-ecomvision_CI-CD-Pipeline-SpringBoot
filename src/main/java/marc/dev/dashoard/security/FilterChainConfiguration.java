dccea48a514021df4793d714e685bba84a2797cb
Marckenley Dorsainvil <marckenlygbeau@gmail.com>
2024-11-16 15:32:37 +0400

    Add new Url for ecomdash host on vercel

--- src/main/java/marc/dev/dashoard/security/FilterChainConfiguration.java
+++ src/main/java/marc/dev/dashoard/security/FilterChainConfiguration.java
@@ -62,7 +62,7 @@ public class FilterChainConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         var corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("http://ecomvision.com", "http://localhost:4200", "http://localhost:3000","https://ecomvdashboard.netlify.app","https://https://ecom-dashfrontend.vercel.app"));
+        corsConfiguration.setAllowedOrigins(List.of("http://ecomvision.com", "http://localhost:4200", "http://localhost:3000","https://ecomvdashboard.netlify.app","https://ecom-dashfrontend.vercel.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS, FILE_NAME));
         corsConfiguration.setExposedHeaders(Arrays.asList(ORIGIN, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCEPT, AUTHORIZATION, X_REQUESTED_WITH, ACCESS_CONTROL_REQUEST_METHOD, ACCESS_CONTROL_REQUEST_HEADERS, ACCESS_CONTROL_ALLOW_CREDENTIALS, FILE_NAME));
         corsConfiguration.setAllowedMethods(Arrays.asList(GET.name(), POST.name(), PUT.name(), PATCH.name(), DELETE.name(), OPTIONS.name()));
